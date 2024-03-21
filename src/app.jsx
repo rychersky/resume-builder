62ec018790639d85a52c503dad7076330c51b4fe
rychersky <rychersky@gmail.com>
2024-03-21 15:23:59 -0500

    resume page refactor

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './app.scss';
 import ContactForm from './components/contact-form/contact-form';
 import EducationForm from './components/education-form/education-form';
+import Resume from './components/resume/resume';
 
 export default function App() {
   const [contactInfo, setContactInfo] = useState({});
@@ -9,49 +10,55 @@ export default function App() {
 
   const [fakeRouter, setFakeRouter] = useState('init contact');
 
-  function initContact(info) {
-    setContactInfo(info);
-    setFakeRouter('init education');
-  }
+  return (
+    <div className="app">
+      <Resume />
+    </div>
+  );
 
-  function initEducation(info) {
-    setEducationInfo([info]);
-    setFakeRouter('init experience');
-  }
+  // function initContact(info) {
+  //   setContactInfo(info);
+  //   setFakeRouter('init education');
+  // }
 
-  if (fakeRouter === 'init contact') {
-    return (
-      <div className="app">
-        <ContactForm initContact={initContact} />
+  // function initEducation(info) {
+  //   setEducationInfo([info]);
+  //   setFakeRouter('init experience');
+  // }
 
-        <button
-          className="print-state"
-          type="button"
-          onClick={() => {
-            console.log(contactInfo);
-            console.log(educationInfo);
-          }}
-        >
-          Print state
-        </button>
-      </div>
-    );
-  } else if (fakeRouter === 'init education') {
-    return (
-      <div className="app">
-        <EducationForm initEducation={initEducation} />
+  // if (fakeRouter === 'init contact') {
+  //   return (
+  //     <div className="app">
+  //       <ContactForm initContact={initContact} />
 
-        <button
-          className="print-state"
-          type="button"
-          onClick={() => {
-            console.log(contactInfo);
-            console.log(educationInfo);
-          }}
-        >
-          Print state
-        </button>
-      </div>
-    );
-  }
+  //       <button
+  //         className="print-state"
+  //         type="button"
+  //         onClick={() => {
+  //           console.log(contactInfo);
+  //           console.log(educationInfo);
+  //         }}
+  //       >
+  //         Print state
+  //       </button>
+  //     </div>
+  //   );
+  // } else if (fakeRouter === 'init education') {
+  //   return (
+  //     <div className="app">
+  //       <EducationForm initEducation={initEducation} />
+
+  //       <button
+  //         className="print-state"
+  //         type="button"
+  //         onClick={() => {
+  //           console.log(contactInfo);
+  //           console.log(educationInfo);
+  //         }}
+  //       >
+  //         Print state
+  //       </button>
+  //     </div>
+  //   );
+  // }
 }
---
+++ src/components/resume/resume.jsx
@@ -0,0 +1,102 @@
+import './resume.scss';
+
+export default function Resume() {
+  return (
+    <div className="resume-container">
+      <div className="resume">
+        <section className="contact-section">
+          <h2>Test User</h2>
+          <div>
+            <p>212-555-1234</p>
+            <p>test@user.com</p>
+          </div>
+          <button className="edit" type="button">
+            Edit
+          </button>
+        </section>
+        <section className="experience-section">
+          <h2>Experience</h2>
+          <ul className="experience-list">
+            <li className="experience">
+              <h3>Test Company #1</h3>
+              <p>2020 - present</p>
+              <p>Software Engineer I</p>
+              <ul>
+                <li>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Alias vero sint repellat ab eos veniam assumenda voluptatibus
+                  accusamus accusantium consequuntur vel cumque harum, facere ad
+                  ea, fuga quidem incidunt nobis?
+                </li>
+                <li>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Alias vero sint repellat ab eos veniam assumenda voluptatibus
+                  accusamus accusantium consequuntur vel cumque harum, facere ad
+                  ea, fuga quidem incidunt nobis?
+                </li>
+                <li>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Alias vero sint repellat ab eos veniam assumenda voluptatibus
+                  accusamus accusantium consequuntur vel cumque harum, facere ad
+                  ea, fuga quidem incidunt nobis?
+                </li>
+                <li>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Alias vero sint repellat ab eos veniam assumenda voluptatibus
+                  accusamus accusantium consequuntur vel cumque harum, facere ad
+                  ea, fuga quidem incidunt nobis?
+                </li>
+              </ul>
+              <button className="edit" type="button">
+                Edit
+              </button>
+              <li>
+                <button className="delete">Delete</button>
+              </li>
+            </li>
+            <li>
+              <button className="add">Add</button>
+            </li>
+          </ul>
+        </section>
+        <section className="education-section">
+          <h2>Education</h2>
+          <ul className="education-list">
+            <li>
+              <h3>Bachelor of Science, Computer Science</h3>
+              <div>
+                <p>University</p>
+                <p className="university-name">University of Test</p>
+              </div>
+              <div>
+                <p>Graduation year</p>
+                <p className="graduation-year">2024</p>
+              </div>
+              <div>
+                <p>GPA</p>
+                <p className="gpa">3.91</p>
+              </div>
+              <div>
+                <p>Notes</p>
+                <p className="notes">Summa Cum Laude, Presidents Club</p>
+              </div>
+
+              <button className="edit" type="button">
+                Edit
+              </button>
+              <button className="delete" type="button">
+                Delete
+              </button>
+            </li>
+            <li>
+              <button className="add">Add</button>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <button className="print" type="button">
+        Print
+      </button>
+    </div>
+  );
+}
