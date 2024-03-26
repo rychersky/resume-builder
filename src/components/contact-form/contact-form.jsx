ae906d88e69fa6f1422ade1480beae5e2dd8915f
rychersky <rychersky@gmail.com>
2024-03-26 15:49:12 -0500

    contact form set up

--- src/components/contact-form/contact-form.jsx
+++ src/components/contact-form/contact-form.jsx
@@ -1,8 +1,37 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import './contact-form.scss';
 
-export default function ContactForm({ initContact }) {
-  const [inputs, setInputs] = useState({});
+export default function ContactForm({
+  contactInfo,
+  isOpen,
+  setContactFormOpen,
+  setContactInfo,
+}) {
+  const [inputs, setInputs] = useState(contactInfo);
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    function handleEsc(e) {
+      if (e.code === 'Escape') {
+        ref.current.close();
+        setContactFormOpen(false);
+      }
+    }
+
+    window.addEventListener('keydown', handleEsc);
+
+    if (isOpen) {
+      ref.current.showModal();
+    } else {
+      ref.current.close();
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, [isOpen, setContactFormOpen, inputs]);
 
   function handleChange(event) {
     const name = event.target.name;
@@ -10,48 +39,64 @@ export default function ContactForm({ initContact }) {
     setInputs((values) => ({ ...values, [name]: value }));
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    initContact(inputs);
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Contact Information</h2>
-
-      <label>Name</label>
-      <input
-        id="name"
-        name="name"
-        type="text"
-        value={inputs.name || ''}
-        onChange={handleChange}
-      />
-
-      <label>Email</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        value={inputs.email || ''}
-        onChange={handleChange}
-      />
-
-      <label>Phone</label>
-      <input
-        id="phone"
-        name="phone"
-        type="tel"
-        value={inputs.phone || ''}
-        onChange={handleChange}
-      />
-
-      <button
-        type="submit"
-        disabled={!inputs.name || !inputs.email || !inputs.phone}
+    <dialog ref={ref}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setContactInfo(inputs);
+          setContactFormOpen(false);
+          ref.current.close();
+        }}
       >
-        Save
-      </button>
-    </form>
+        <h2>Contact Information</h2>
+
+        <label>Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+        <label>Email</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          value={inputs.email}
+          onChange={handleChange}
+        />
+
+        <label>Phone</label>
+        <input
+          id="phone"
+          name="phone"
+          type="text"
+          value={inputs.phone}
+          onChange={handleChange}
+        />
+
+        <div>
+          <button
+            className="save"
+            type="submit"
+            disabled={!inputs.name || !inputs.email || !inputs.phone}
+          >
+            Save
+          </button>
+          <button
+            className="cancel"
+            type="button"
+            onClick={() => {
+              ref.current.close();
+              setContactFormOpen(false);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </dialog>
   );
 }
